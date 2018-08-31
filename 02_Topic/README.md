6bf72ff509af46386401aa26ffab225a19bfc9d8
Wellington Umbelino <41035606+wellingtonumbelino@users.noreply.github.com>
2018-08-31 12:26:08 -0300

    Add files via upload

---
+++ 02_Topic/README.md
@@ -0,0 +1,19 @@
+# 02_Topic
+-	O que voce fez?
+
+    * Fiz um programa de uma topic de luxo, onde o usuário inicializa a topic dizendo quantos lugares ela tem no total mais a quantidade de assentos prefenciais. O usuário embarca pessoas, e de acordo com a idade ela é alocada em um assento preferencial ou não. Se o elemento tiver mais que 60 anos ele é alocado no assento preferencial, caso os assentos preferenciais já estejam ocupados, ele é alocado no próximo assento vago, se tiver menos que 60 é alocado em um assento normal, e só será alocado no assento preferencial se os outros assentos já estiverem ocupados. O programa também desembarca as pessoas passando por parâmetro o nome do indivíduo, caso não esteja na topic é emitido uma mensagem de erro. E uma pessoa também não pode embarcar duas vezes.
+
+-	Com quem voce fez?
+
+    * Eu fiz com a ajuda do Abílio que é do PET TI, ele foi me explicando os casos que precisava fazer, mas não entendi muito bem o que ele me explicou, mas estarei revisando o código analisando linha por linha para entender.
+-	O que aprendeu e sabe fazer?
+
+    * Consegui entender mais o vector e o toString, aprendi a usa-los no código e entendi mais como se usar ponteiro. Sei fazer o construtor e as struct.
+
+-	O que tem dificuldade ainda?
+
+    * Tenho dificuldade em entender qual a forma de fazer o código, também não sei muito C++ e fico perdido sem saber o que fazer.
+
+-	Quanto tempo levou pra fazer a atividade?
+
+    * Levei em torno de 2h, com o Abílio me ajudando a fazer e a gente tentando enteder como fazer.
---
+++ 02_Topic/topic.cpp
@@ -0,0 +1,182 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <cstdio>
+using namespace std;
+
+struct Pass{
+    nome : string;
+    idade : int;
+    tipo : string;
+    bool existe;
+
+    Pass(string nome = "fulano", int idade = 0){
+        this->nome = nome;
+        this->idade = idade;
+        this->existe = false;
+        this->tipo = "#";
+    }
+    
+    void setTipo(string tipo){
+        this->tipo = tipo;
+    }
+
+    string toString(){
+        stringstream ss;
+        ss << this->tipo << this-> nome << ":" << this-> idade;
+        return ss.str();
+    }
+};
+
+
+struct Topic{
+    int qtd;
+    int qtdIdoso;
+     passageiros : vector<Pass*>;
+
+    Topic(int qtd = 0, int qtdIdoso = 0){
+        this-> qtd = qtd;
+        this-> qtdIdoso = qtdIdoso;
+        for(int i = 0; i < qtd; i++){
+            Pass * passageiro = new Pass("",0);
+            passageiros.push_back(passageiro);
+        }
+        for(int i = 0; i < qtdIdoso; i++){
+            passageiros[i]->setTipo("@");
+        }
+    }
+
+    string toString(){
+        stringstream ss;
+        ss << "[";
+        for(Pass * passageiro : passageiros){
+            if(passageiro->existe){
+                ss << " " << passageiro->toString() << " ";
+            }else{
+                ss << " " << passageiro->tipo << " ";
+            }
+        }
+        ss << "]";
+        return ss.str();
+    }
+
+    bool novo(Pass * passageiro){
+       for(Pass * pass : passageiros){
+           if(pass == nullptr){
+               passageiros.push_back(passageiro);
+               return true;
+           }
+       }
+    }
+
+    bool in(string nome, int idade){
+        Pass * passageiro = new Pass(nome, idade);
+        if(this->qtd == 0 && this->qtdIdoso == 0){
+            puts("fail: topic nao inicializada!");
+            return false;
+        }
+        if(idade > 60){
+            for(int i = 0; i < qtdIdoso; i++){
+                if(!passageiros[i]->existe){
+                    passageiros[i] = passageiro;
+                    passageiro->setTipo("@");
+                    passageiro->existe = true;
+                    cout << "done" << endl;
+                    return true;
+                }else{
+                    if(passageiros[i]->nome == passageiro->nome){
+                        cout << "fail: " << passageiro->nome << " ja esta na topic" << endl;
+                        return false;
+                    }
+                }
+            }
+            for(int i = qtdIdoso; i < qtd; i++){
+                if(!passageiros[i]->existe){
+                    passageiros[i] = passageiro;
+                    passageiro->existe = true;
+                    cout << "done" << endl;
+                    return true;
+                }else{
+                    if(passageiros[i]->nome == passageiro->nome){
+                        cout << "fail: " << passageiro->nome << " ja esta na topic" << endl;
+                        return false;
+                    }
+                }
+            }
+            cout << "fail: topic lotada" << endl;
+            return false;
+        }else{
+            for(int i = qtdIdoso; i < qtd; i++){
+                if(!passageiros[i]->existe){
+                    passageiros[i] = passageiro;
+                    passageiro->existe = true;
+                    cout << "done" << endl;
+                    return true;
+                }else{
+                    if(passageiros[i]->nome == passageiro->nome){
+                        cout << "fail: " << passageiro->nome << " ja esta na topic" << endl;
+                        return false;
+                    }
+                }
+            }
+            for(int i = 0; i < qtdIdoso; i++){
+                if(!passageiros[i]->existe){
+                    passageiros[i] = passageiro;
+                    passageiro->setTipo("@");
+                    passageiro->existe = true;
+                    cout << "done" << endl;
+                    return true;
+                }else{
+                    if(passageiros[i]->nome == passageiro->nome){
+                        cout << "fail: " << passageiro->nome << " ja esta na topic" << endl;
+                        return false;
+                    }
+                }
+            }
+            cout << "fail: topic lotada" << endl;
+            return false;
+        }
+    }
+
+    bool out(string nome){
+        for(int i = 0; i < qtd; i++){
+            if(passageiros[i]->existe && passageiros[i]->nome == nome){
+                delete passageiros[i];
+                passageiros[i] = nullptr;
+                cout << "done" << endl;
+                return true;
+            }
+        }
+        cout << "fail: " << nome << " nao esta na topic" << endl;
+        return false;
+    }
+};
+
+int main()
+{
+    Topic topic;
+    string op;
+    while(true){
+        cin >> op;
+        if(op == "new"){
+            int total, idoso;
+            cin >> total >> idoso;
+            topic = Topic(total, idoso);
+            cout << "done" << endl;
+        }else if(op == "show"){
+            cout << topic.toString() << endl;
+        }else if(op == "in"){
+            string nome;
+            int idade;
+            cin >> nome >> idade;
+            topic.in(nome, idade);
+        }else if(op == "out"){
+            string nome;
+            cin >> nome;
+            topic.out(nome);
+        }else if(op == "end"){
+            break;
+        }
+    }
+    return 0;
+}
